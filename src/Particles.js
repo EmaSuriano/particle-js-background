845de6933b5bd6fbd0cec7cc9320676c1f97bb64
Emanuel Suriano <emanuel.suriano@gmail.com>
2018-03-21 17:36:49 -0300

    increase the amount of particles

--- src/Particles.js
+++ src/Particles.js
@@ -1,19 +1,19 @@
-import React, { Component } from "react";
-import Particles from "react-particles-js";
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 
 class Canvas extends Component {
-  state = { width: "0px", height: "0px" };
+  state = { width: '0px', height: '0px' };
   componentDidMount() {
     this.updateWindowDimensions();
-    window.addEventListener("resize", this.updateWindowDimensions);
+    window.addEventListener('resize', this.updateWindowDimensions);
   }
   componentWillUnmount() {
-    window.removeEventListener("resize", this.updateWindowDimensions);
+    window.removeEventListener('resize', this.updateWindowDimensions);
   }
   updateWindowDimensions = () => {
     this.setState({
       width: `${window.innerWidth}px`,
-      height: `${window.innerHeight}px`
+      height: `${window.innerHeight}px`,
     });
   };
   render() {
@@ -25,20 +25,25 @@ class Canvas extends Component {
         params={{
           particles: {
             number: {
-              value: 50,
+              value: 75,
               density: {
                 enable: true,
-                value_area: 800
-              }
+                value_area: 800,
+              },
             },
             color: {
-              value: "#ffffff"
+              value: '#ffffff',
             },
             shape: {
-              type: "circle",
+              type: 'circle',
               stroke: {
                 width: 0,
-                color: "#000000"
+                color: '#000000',
+              },
+              image: {
+                src: 'img/github.svg',
+                width: 100,
+                height: 100,
               },
             },
             opacity: {
@@ -48,8 +53,8 @@ class Canvas extends Component {
                 enable: true,
                 speed: 1,
                 opacity_min: 0.1,
-                sync: false
-              }
+                sync: false,
+              },
             },
             size: {
               value: 5,
@@ -58,49 +63,49 @@ class Canvas extends Component {
                 enable: true,
                 speed: 2,
                 size_min: 3,
-                sync: false
-              }
+                sync: false,
+              },
             },
             line_linked: {
               enable_auto: true,
               distance: 100,
-              color: "#fff",
+              color: '#fff',
               opacity: 1,
               width: 1,
               condensed_mode: {
                 enable: false,
                 rotateX: 600,
-                rotateY: 600
-              }
+                rotateY: 600,
+              },
             },
             move: {
               enable: true,
               speed: 1,
-              direction: "none",
+              direction: 'none',
               random: false,
               straight: false,
-              out_mode: "out",
+              out_mode: 'out',
               bounce: false,
               attract: {
                 enable: false,
                 rotateX: 600,
-                rotateY: 1200
-              }
-            }
+                rotateY: 1200,
+              },
+            },
           },
           interactivity: {
-            detect_on: "canvas",
+            detect_on: 'canvas',
             events: {
               onhover: {
-                enable: false
+                enable: false,
               },
               onclick: {
-                enable: false
+                enable: false,
               },
-              resize: true
-            }
+              resize: true,
+            },
           },
-          retina_detect: true
+          retina_detect: true,
         }}
       />
     );
