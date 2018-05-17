395a8e0669d97c40a89cd1e00b44106fa98e0ed9
Emanuel Suriano <emanuel.suriano@gmail.com>
2018-05-17 01:07:57 -0300

    Adding particle configuration via url!

--- src/Particles.js
+++ src/Particles.js
@@ -3,96 +3,31 @@ import Particles from 'react-particles-js';
 
 class Canvas extends Component {
   state = { width: '0px', height: '0px' };
+
   componentDidMount() {
     this.updateWindowDimensions();
     window.addEventListener('resize', this.updateWindowDimensions);
   }
+
   componentWillUnmount() {
     window.removeEventListener('resize', this.updateWindowDimensions);
   }
+
   updateWindowDimensions = () => {
     this.setState({
       width: `${window.innerWidth - 5}px`,
       height: `${window.innerHeight - 5}px`,
     });
   };
+
   render() {
     const { width, height } = this.state;
-    console.log(width, height);
+    const { particles } = this.props;
     return (
       <Particles
         {...this.state}
         params={{
-          particles: {
-            number: {
-              value: 50,
-              density: {
-                enable: true,
-                value_area: 800,
-              },
-            },
-            color: {
-              value: '#ffffff',
-            },
-            shape: {
-              type: 'circle',
-              stroke: {
-                width: 0,
-                color: '#000000',
-              },
-              image: {
-                src: 'img/github.svg',
-                width: 100,
-                height: 100,
-              },
-            },
-            opacity: {
-              value: 1,
-              random: true,
-              anim: {
-                enable: true,
-                speed: 1,
-                opacity_min: 0.1,
-                sync: false,
-              },
-            },
-            size: {
-              value: 5,
-              random: true,
-              anim: {
-                enable: true,
-                speed: 2,
-                size_min: 3,
-                sync: false,
-              },
-            },
-            line_linked: {
-              enable_auto: true,
-              distance: 100,
-              color: '#fff',
-              opacity: 1,
-              width: 1,
-              condensed_mode: {
-                enable: false,
-                rotateX: 600,
-                rotateY: 600,
-              },
-            },
-            move: {
-              enable: true,
-              speed: 1,
-              direction: 'none',
-              random: false,
-              straight: false,
-              out_mode: 'out',
-              bounce: false,
-              attract: {
-                enable: false,
-                rotateX: 600,
-                rotateY: 1200,
-              },
-            },
-          },
+          particles,
           interactivity: {
             detect_on: 'canvas',
             events: {
--- src/index.js
+++ src/index.js
@@ -1,24 +1,34 @@
-import React from "react";
-import { render } from "react-dom";
-import Particles from "./Particles";
-import "./index.css";
+import React from 'react';
+import { render } from 'react-dom';
+import Particles from './Particles';
+import { addUrlProps, UrlQueryParamTypes } from 'react-url-query';
+import styled from 'styled-components';
+import './index.css';
+import particleType from './particleType';
 
-const styles = {
-  root: {
-    fontFamily: "sans-serif",
-    textAlign: "center",
-    height: "100%",
-    background: "#222",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center"
-  }
+const urlPropsQueryConfig = {
+  background: { type: UrlQueryParamTypes.string, defaultValue: '#222' },
+  particles: { type: particleType },
 };
 
-const App = () => (
-  <div style={styles.root}>
-    <Particles />
-  </div>
+const AppContainer = styled.div`
+  font-family: sans-serif;
+  text-align: center;
+  height: 100%;
+  background: ${props => props.background};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const App = ({ background, particles }) => (
+  <AppContainer background={background}>
+    <Particles particles={particles} />
+  </AppContainer>
 );
 
-render(<App />, document.getElementById("root"));
+const AppWithUrlParameters = addUrlProps({ urlPropsQueryConfig })(App);
+
+render(<AppWithUrlParameters />, document.getElementById('root'));
+
+// to send particles you have to perform encodeURIComponent(JSON.stringify(PARTICLES_CONF))
